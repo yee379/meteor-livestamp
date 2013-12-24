090e0829315597587220ec88de038618912fa433
Collin Donahue-Oponski <collindo@gmail.com>
2013-12-24 01:19:53 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Livestamp.js for Meteor
+
+[Livestamp.js](http://mattbradley.github.io/livestampjs/), a simple, unobtrusive jQuery plugin that provides auto-updating timeago text to your timestamped HTML elements using Moment.js, packaged for Meteor.
+
+Installation
+-------------
+
+`mrt add livestamp`
+
+Usage
+-------------
+Just like the [Livestamp docs](http://mattbradley.github.io/livestampjs/) tell you:
+
+`<span data-livestamp="2013-12-24T08:14:42.345Z"></span>`
---
+++ package.js
@@ -0,0 +1,12 @@
+Package.describe({
+  summary: "Livestamp.js, a simple, unobtrusive jQuery plugin that provides auto-updating timeago text to your timestamped HTML elements using Moment.js. See http://mattbradley.github.io/livestampjs/"
+});
+
+Package.on_use(function (api, where) {
+  if(api.export) {
+    api.export('livestamp');
+  }
+  where = where || ['client'];
+  api.add_files('lib/livestamp/livestamp.js', where);
+  api.add_files('export-livestamp.js', where);
+});
